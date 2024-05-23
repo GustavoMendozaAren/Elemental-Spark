5d35fbb08a2039773fb163598030917fec4cd3c5
GustavoMendozaAren <stavmdza@gmail.com>
2024-05-22 23:33:28 -0600

    MuchosAvances

--- ElementalSpark/Assets/3-Scripts/1-PlayerScripts/PlayerMovement.cs
+++ ElementalSpark/Assets/3-Scripts/1-PlayerScripts/PlayerMovement.cs
@@ -20,12 +20,19 @@ public class PlayerMovement : MonoBehaviour
 
     // GroundCheck transform variable
     public Transform groundCheck;
+    private bool isGrounded;
+
 
     void Awake()
     {
         // Initializing rigid body variable
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        CheckOnGround();
     }
 
     void FixedUpdate()
@@ -60,6 +67,16 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
 
+    void CheckOnGround() 
+    {
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.4f, LayerMask.GetMask("Ground"));
+
+        if (isGrounded) 
+        {
+            anim.SetBool("Jumping", false);
+        }
+    }
+
     void PlayerJump()
     {
         // Check for jump input
@@ -72,8 +89,16 @@ public class PlayerMovement : MonoBehaviour
             {
                 // Apply jump force vertically using Rigidbody2D
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                anim.SetBool("Jumping", true);
             }
+
         }
+    }
+
+    // Player atack ability
+    public void PlayerAtackButton()
+    {
+        anim.SetTrigger("Atack");
     }
 
     void ChangeDirection(int direction)
@@ -87,4 +112,6 @@ public class PlayerMovement : MonoBehaviour
     {
         jumped = true;
     }
+
+    
 }
